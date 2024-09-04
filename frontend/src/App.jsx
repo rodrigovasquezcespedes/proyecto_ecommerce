46beb90670804b4df9334fa81fa52a76d6f31ff5
Romina Osorio <r.osorio.marmolejo@gmail.com>
2024-09-03 21:40:58 -0400

    crea contextos y rutas protegidas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,56 @@
-import React from 'react'
+import Navigation from './components/Navigation'
+import { Routes, Route } from 'react-router-dom'
+import { Home, Profile, NotFound, Shopping, Products, Login, Detail, Favorites, Register } from './views'
+import Footer from './components/Footer'
+import ProtectedRoute from './ProtectedRoute'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Navigation />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route
+          path='/productos' element={
+            <ProtectedRoute>
+              <Products />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/productos/:id' element={
+            <ProtectedRoute>
+              <Detail />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/profile' element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/favoritos' element={
+            <ProtectedRoute>
+              <Favorites />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/carrito' element={
+            <ProtectedRoute>
+              <Shopping />
+            </ProtectedRoute>
+          }
+        />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './main.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { HashRouter } from 'react-router-dom'
+import TecnoProvider from './context/TecnoContext.jsx'
+import AuthProvider from './context/AuthContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <AuthProvider>
+        <TecnoProvider>
+          <App />
+        </TecnoProvider>
+      </AuthProvider>
+    </HashRouter>
   </React.StrictMode>
 )
